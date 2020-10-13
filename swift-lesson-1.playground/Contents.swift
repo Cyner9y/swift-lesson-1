57987137baca8118912a4b3cb6555f2e10677220
Yuriy Fedorov <>
2020-10-13 23:55:33 +0300

    fix after review

--- swift-lesson-1.playground/Contents.swift
+++ swift-lesson-1.playground/Contents.swift
@@ -2,9 +2,9 @@ import UIKit
 
 //Задание №1 - Решить квадратное уравнение
 
-let a = Double(1)
-let b = Double(8.5)
-let c = Double(16)
+let a = 1.0
+let b = 8.5
+let c = 16.0
 
 let discr = pow(b, 2) - (4 * a * c)
 
@@ -22,8 +22,8 @@ if discr > 0 {
 
 //Задание №2 - Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
 
-let catet1: Double = 5.5
-let catet2: Double = 10
+let catet1 = 5.5
+let catet2 = 10.0
 
 if catet1 < 0 || catet2 < 0 {
     print("Катеты должны быть положительными числами")
@@ -39,8 +39,8 @@ if catet1 < 0 || catet2 < 0 {
 
 //Задание №3 - Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет
 
-let depositSum: Double = 10000
-let percent: Double = 15.1
+let depositSum = 10000.00
+let percent = 15.1
 let years = 5
 var totalDepositSum = depositSum + depositSum * (percent/100)
 
